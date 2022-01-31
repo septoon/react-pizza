3b3d81540243f0dc33c052881dcde06ce249d59f
septoon <tigran.darchinyan@gmail.com>
2022-01-31 20:23:24 +0200

    use hooks instead of component container

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header/Header';
 import Catalogue from './components/Catalogue/Catalogue';
 import TeaCardContainer from './components/TeaCard/TeaCardContainer';
 import AboutUs from './components/AboutUs/AboutUs';
-import DeliveryContainer from './components/Delivery/DeliveryContainer';
+import Delivery from './components/Delivery/Delivery';
 import Main from './components/Main/Main';
 import CartContainer from './components/Cart/Cart.Container';
 import ContactUs from './components/emailjs/EmailForm';
@@ -21,7 +21,7 @@ const App = () => {
         <Route path='/catalog' component={ Catalogue } />
         <Route path='/teaCard' component={ TeaCardContainer } />
         <Route path='/aboutUs' component={ AboutUs } />
-        <Route path='/delivery' component={ DeliveryContainer } />
+        <Route path='/delivery' component={ Delivery } />
         <Route path='/cart' component={ CartContainer } />
         <Route path='/order' component={ ContactUs } />
       </div>
--- src/components/Delivery/Delivery.jsx
+++ src/components/Delivery/Delivery.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import './Delivery.css'
+import {useSelector, useDispatch} from 'react-redux';
+
+import { deliveryList } from '../../common/json/delivery-list'
+import { addDeliveryList } from '../../redux/delivery-reducer'
 
 const DeliveryPage = (props) => {
+  const dispatch = useDispatch()
+  const { list } = useSelector(({ deliveryPage}) => ({
+    list: deliveryPage.deliveryListData
+  }))
+
+  React.useEffect(() => { 
+    dispatch(addDeliveryList(deliveryList))
+  }, [])
+
   return (
     <div className='delivery-main'>
       <div className='delivery-list'>
         <h1>Доставка</h1>
         <table border="0">
         <tbody>
-          {props.list.map(i => {
+          {list.map(i => {
             return(
               <tr>
                 <td>{i.city}</td>

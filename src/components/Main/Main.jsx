2cdb4d127cceac541c127260fd0409b3cc5a736b
Tigran <41443306+septoon@users.noreply.github.com>
2023-04-07 21:27:06 +0300

    main order button

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,15 +4,27 @@ import basket from '../../common/img/shopping-cart.svg'
 import './css/Main.css'
 import './css/Main-media.css'
 import { NavLink } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { toggleIsActiveAC } from '../../redux/nav-reducer'
 
 const Main = props => {
+  const dispatch = useDispatch()
+
+  React.useEffect(() => { 
+    dispatch(toggleIsActiveAC(false))
+  }, [dispatch])
+
+  const handleClick = (el) => {
+    dispatch(toggleIsActiveAC(true))
+  }
+
   return (
     <div className='container main'>
       <div className='text'>
         <h1>Делись любовью с помощью пиццы</h1>
       </div>
       <NavLink to='/catalog' >
-        <button className="order">
+        <button className="order" onClick={ () => handleClick() }>
           <img src={basket} alt="basket" className="basket-main"/>
           Заказать онлайн
         </button>

df091d34de74eb3b0477515f3cf1067d74494fae
Tigran <41443306+septoon@users.noreply.github.com>
2023-04-11 22:15:44 +0300

    undo "setTimeout"

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import {store} from './redux/redux-store'
 import {Provider} from 'react-redux';
 
-setTimeout(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store} >     
-          <App />
-        </Provider> 
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}, 1000)
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store} >     
+        <App />
+      </Provider> 
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);

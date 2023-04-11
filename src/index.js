88b26991d48cbc28076e6b8346100c10971b1a1a
Tigran <41443306+septoon@users.noreply.github.com>
2023-04-11 21:55:17 +0300

    added setTimeout

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import {store} from './redux/redux-store'
 import {Provider} from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store} >     
-        <App />
-      </Provider> 
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+setTimeout(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Provider store={store} >     
+          <App />
+        </Provider> 
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000)
